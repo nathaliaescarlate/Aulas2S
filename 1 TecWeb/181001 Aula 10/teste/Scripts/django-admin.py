9f2ebfad6e446537b6351bfe810291504772b240
Nathalia Castilho Escarlate <nathaliaescarlate@hotmail.com>
2018-10-01 11:43:26 -0300

    django

---
+++ 1 TecWeb/181001 Aula 10/teste/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\1800179\docume~1\etalra~1\aulas2s\1tecwe~1\181001~1\teste\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
